4565d1dc5288a6ab1702864331d13362c719d75d
fatemeh <f.robatjazi.1380@gmail.com>
2022-07-11 18:48:42 +0430

    Run Laravel Mix, Add The Font And Fontawesome Folder

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,29 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+const btn = document.getElementById("btn");
+const main = document.getElementById("main");
+const text = document.getElementById("text");
 
-require('./bootstrap');
+function AddFilter() {
+  main.classList.add("filter");
+  
+  btn.querySelector('i.fa-solid').classList.remove('fa-eye-slash');
+  btn.querySelector('i.fa-solid').classList.add('fa-eye');
+  text.innerText = "نمایش بده";
+}
 
-window.Vue = require('vue').default;
+function RemoveFilter() {
+  main.classList.remove("filter");
+  
+  btn.querySelector('i.fa-solid').classList.remove('fa-eye');
+  btn.querySelector('i.fa-solid').classList.add('fa-eye-slash');
+  text.innerText = "مخفی کن";
+}
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+btn.addEventListener('click', () => {
+  const isfilter = main.classList.contains('filter');
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
+  if (isfilter) {
+    RemoveFilter();
+  } else {
+    AddFilter();
+  }
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//     .vue()
+//     .sass('resources/sass/app.scss', 'public/css');
+
+
+mix.styles([
+    'resources/css/bootstrap.css',
+    'resources/css/app.css',
+], 'public/css/all.css');
+
+
+mix.scripts([
+    'resources/js/app.js',
+], 'public/js/all.js');
