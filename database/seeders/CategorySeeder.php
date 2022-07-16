7ead78ac29b5b9c52c029211b5699fcb9cade392
fatemeh <f.robatjazi.1380@gmail.com>
2022-07-16 12:14:47 +0430

    Making Migration categories, making Seeder And Giving Fake Data

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            [
+                "id" => 1,
+                "title" => "Open",
+            ]
+        ]);
+
+        DB::table('categories')->insert([
+            [
+                "id" => 2,
+                "title" => "Done",
+            ]
+        ]);
+    }
+}
